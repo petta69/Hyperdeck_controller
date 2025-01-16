a3b3eff3032ee0ca47b49eccf8fb31ebf9ec6ab6
Peter <stridsberg.peter@gmail.com>
2025-01-16 14:59:12 +0100

    Minor updates

--- artnet_service.py
+++ artnet_service.py
@@ -8,12 +8,13 @@ logger = Logger(name=__name__, level=verbose).get_logger()
 def main():
     logger.debug("Starting")
     hyperdeck = Hyperdeck(port=9993, host_ip='192.168.197.22')
-    artNet = Artnet.Artnet()
+    artNet = Artnet.Artnet(DEBUG=True, SYSIP="192.168.111.246")
     
     while True:
         artNetPacket = artNet.readBuffer()[0]
         dmxPacket = artNetPacket.data
-        logger.debug(f"Data: {dmxPacket}")
+        if dmxPacket is not None:
+            logger.debug(f"Data: {dmxPacket}")
     
     
 
